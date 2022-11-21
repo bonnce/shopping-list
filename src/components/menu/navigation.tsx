12bdfd37adfafee4d883b6641c1f349926bca1bb
bonnce <bonce_reloko@hotmail.com>
2022-11-21 01:44:47 -0300

    gh config

--- src/components/menu/navigation.tsx
+++ src/components/menu/navigation.tsx
@@ -2,7 +2,7 @@ import BaseLink from "./baseLink"
 
 const Navigation = ({handleLinkClick} : {handleLinkClick:VoidFunction}) => {
     return <div className='container column align-start gap-md nav'>
-        <BaseLink to='(/)' onClick={handleLinkClick} >Agregar producto</BaseLink>
+        <BaseLink to='' onClick={handleLinkClick} >Agregar producto</BaseLink>
         <BaseLink to='/buildList' onClick={handleLinkClick} >Generar lista</BaseLink>
         <BaseLink to='/adminProducts' onClick={handleLinkClick} >Administrar productos</BaseLink>
         <BaseLink to='/install' onClick={handleLinkClick}>Guia de instalacion</BaseLink>
